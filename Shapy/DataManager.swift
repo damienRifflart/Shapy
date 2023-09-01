ca2273ff7c82ea55533c9d90bde84a3537edb9a6
ooDams <101669514+ooDams@users.noreply.github.com>
2023-09-01 10:39:45 +0200

    Added proper interface and finished settings tab

--- Shapy/DataManager.swift
+++ Shapy/DataManager.swift
@@ -2,8 +2,8 @@ import SwiftUI
 
 class DataManager: ObservableObject {
     @Published var currentSerie: Int = 1
-    @Published var exerciceOne: String = "First"
-    @Published var exerciceTwo: String = ""
+    @Published var exerciceOne: String = "15 Pompes"
+    @Published var exerciceTwo: String = "30 Abdos"
     @Published var exerciceThree: String = ""
     @Published var exerciceFour: String = ""
     @Published var exerciceIndex: Int = 0
--- Shapy/SettingsView.swift
+++ Shapy/SettingsView.swift
@@ -1,20 +1,19 @@
-//
-//  SettingsView.swift
-//  Shapy
-//
-//  Created by Damien Rifflart on 31/08/2023.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
     @ObservedObject var variables: DataManager
     @State private var isLandingViewActive = false
     @State private var exerciceOneInput = ""
+    @State private var exerciceTwoInput = ""
+    @State private var exerciceThreeInput = ""
+    @State private var exerciceFourInput = ""
     
     func saveButton(){
         isLandingViewActive.toggle()
         variables.exerciceOne = exerciceOneInput
+        variables.exerciceTwo = exerciceTwoInput
+        variables.exerciceThree = exerciceThreeInput
+        variables.exerciceFour = exerciceFourInput
     }
     
     var body: some View {
@@ -24,12 +23,10 @@ struct SettingsView: View {
             Color(hex: 0x0E0215)
                 .ignoresSafeArea()
             
-
             VStack{
                 HStack{
                     // Back Button
-                    Button(action: {isLandingViewActive.toggle()
-                    }) {
+                    Button(action: { isLandingViewActive.toggle() }) {
                         Text("Retour")
                             .font(.system(size: 30, weight: .bold))
                             .foregroundColor(Color(hex: 0x0E0215))
@@ -41,35 +38,102 @@ struct SettingsView: View {
                     .padding(.leading, 20)
                     
                     Spacer()
+                    
+                    // Save Button
+                    Button(action: { saveButton() }) {
+                        Text("Enregistrer")
+                            .font(.system(size: 30, weight: .bold))
+                            .foregroundColor(Color(hex: 0x0E0215))
+                            .padding()
+                            .frame(width: 188, height:60)
+                            .background(Color(hex: 0xE6D2F7))
+                            .cornerRadius(10)
+                    }
+                    .padding(.trailing, 20)
+                    
+                }
+                .fullScreenCover(isPresented: $isLandingViewActive) {
+                    LandingView(variables: variables)
                 }
                 
                 Spacer()
                 
-                TextField("Enter Exercice One Here", text: $exerciceOneInput)
-                    .foregroundColor(Color.red)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                // Exercice 1
+                Text("Exercice 1")
+                    .foregroundColor(Color.white)
+                    .font(.system(size:35, weight:.bold))
+                    .padding(.leading, 40.0)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                TextField(" 15 Pompes", text: $exerciceOneInput)
+                    .background(Color.gray.opacity(0.2))
+                    .foregroundColor(Color(hex: 0xE6D2F7))
+                    .frame(width: 300)
+                    .cornerRadius(6)
+                    .font(.system(size:40))
+                    .padding(.top, -20)
+                    .padding(.bottom, 20)
                 
-                Spacer()
                 
-                // Start Button
-                Button(action: {saveButton()
-                }) {
-                    Text("Save")
-                        .font(.system(size: 30, weight: .bold))
-                        .foregroundColor(Color(hex: 0x0E0215))
-                        .padding()
-                        .frame(width: 300, height:60)
-                        .background(Color(hex: 0xE6D2F7))
-                        .cornerRadius(10)
-                }
-                .padding(.bottom, 20)
+                // Exercice 2
+                Text("Exercice 2")
+                    .foregroundColor(Color.white)
+                    .font(.system(size:35, weight:.bold))
+                    .padding(.leading, 40.0)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                TextField(" 15 Pompes", text: $exerciceTwoInput)
+                    .background(Color.gray.opacity(0.2))
+                    .foregroundColor(Color(hex: 0xE6D2F7))
+                    .frame(width: 300)
+                    .cornerRadius(6)
+                    .font(.system(size:40))
+                    .padding(.top, -20)
+                    .padding(.bottom, 20)
+                
+                
+                // Exercice 3
+                Text("Exercice 3")
+                    .foregroundColor(Color.white)
+                    .font(.system(size:35, weight:.bold))
+                    .padding(.leading, 40.0)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                TextField(" 15 Pompes", text: $exerciceThreeInput)
+                    .background(Color.gray.opacity(0.2))
+                    .foregroundColor(Color(hex: 0xE6D2F7))
+                    .frame(width: 300)
+                    .cornerRadius(6)
+                    .font(.system(size:40))
+                    .padding(.top, -20)
+                    .padding(.bottom, 20)
+                
+                
+                // Exercice 4
+                Text("Exercice 4")
+                    .foregroundColor(Color.white)
+                    .font(.system(size:35, weight:.bold))
+                    .padding(.leading, 40.0)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                TextField(" 15 Pompes", text: $exerciceFourInput)
+                    .background(Color.gray.opacity(0.2))
+                    .foregroundColor(Color(hex: 0xE6D2F7))
+                    .frame(width: 300)
+                    .cornerRadius(6)
+                    .font(.system(size:40))
+                    .padding(.top, -20)
+                    
+                
+                
             }
-            .fullScreenCover(isPresented: $isLandingViewActive) {
-                LandingView(variables: variables)
-            }
+            .padding(.bottom, 50.0)
+            
+            
         }
     }
 }
+
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
